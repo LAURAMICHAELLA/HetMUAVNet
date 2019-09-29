8c8483f5eaeea2356525b6d09a8a1415eee18469
Laura Ribeiro <laura.michaella@gmail.com>
2019-09-29 12:11:46 -0300

    script of cmake initialization

--- build.sh
+++ build.sh
@@ -2,15 +2,8 @@
 cd "$(dirname "$0")"
 THISDIR="$PWD"
 
-mkdir tmp
-( cd ../.. && git archive HEAD --prefix=gzuav-src/ -o "$THISDIR/tmp/gzuav-src.tar" )
-
-docker build . -t gzuav/build-image
-
-docker run --rm -v "$THISDIR/tmp":/gzuav_tmp gzuav/build-image bash -c '
-	set -ex
-	tar xvf /gzuav_tmp/gzuav-src.tar
-	cd gzuav-src
-	dpkg-buildpackage -b
-	cp -v ../* --target-directory=/gzuav_tmp/
-'
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../install
+make
+make install'
