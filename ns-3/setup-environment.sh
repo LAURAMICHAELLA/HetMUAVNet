55c527fb90988d4a3427854044c0086224a9fdee
Laura Ribeiro <laura.michaella@gmail.com>
2019-09-29 15:22:09 -0300

    setup-environment_v2.0

--- ns-3/setup-environment.sh
+++ ns-3/setup-environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # abort on error
 
-cd ../../ns-3-allinone/ns-3.29/src
+cd ../../../ns-3-allinone/ns-3.29/src
 ln -s ../../../external-sync ./
 
 export PATH=/HetMUAVNet/HetMUAVNet/install/bin:/ns-3-allinone/ns-3.29/build/src/external-sync/examples:$PATH
